3de59b0fd037e1fec970ef0f17ef885ad1be8262
unknown <amitghosh.in2050@gmail.com>
2026-03-16 01:01:05 +0530

    created project structures

---
+++ batch-foundation/src/main/java/com/example/batchfoundation/BatchFoundationApplication.java
@@ -0,0 +1,13 @@
+package com.example.batchfoundation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchFoundationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchFoundationApplication.class, args);
+    }
+
+}
